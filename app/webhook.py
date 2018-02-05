65549a5bb8ca149bc17090888a2b9f8abe3fda92
Marc Harvey <marcharvey1470@gmail.com>
2018-02-05 10:50:01 -0600

    adding logging

--- app/webhook.py
+++ app/webhook.py
@@ -5,18 +5,20 @@ import json
 def get_last_word(json_dict):
     full_bar = json_dict['queryResult']['queryText']
     word_list = full_bar.split()
+    print('getting last word...')
+    print('last word is:' + word_list[-1])
     return word_list[-1]
 
 
 def respond(json_dict):
     last_word = get_last_word(json_dict)
+    print('responding...')
     return make_fulfillment(get_best_word(get_rhyme_words(last_word)))
 
 
 def make_fulfillment(word):
     """ returns a json str containing the repsonse text for dialogflow"""
     response_dict = {'fulfillmentText': word}
+    print('making fulfillment...')
+    print(json.dumps(response_dict))
     return json.dumps(response_dict)
-
-
-print(make_fulfillment(get_best_word(get_rhyme_words('hello'))))
