0bfae86b388aa68f3492050619405547ed655bfd
Marc Harvey <marcharvey1470@gmail.com>
2018-02-05 00:19:35 -0600

    hopefully this does it

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("/templates/index.html")
+    return render_template("index.html")
 
 
 @app.route('/webhook', methods=['POST'])
